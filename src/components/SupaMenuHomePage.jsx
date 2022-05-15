c6674644ffcb0411c447a3eb620a7b6f37484bb8
ernesteNtezirizaza <erneste.ntezirizaza@gmail.com>
2022-05-15 14:20:41 +0200

    project init

---
+++ src/components/SupaMenuHomePage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import {View, Text, TextInput} from "react-native";
+
+const SupaMenuHomePage = () => {
+    <View>
+        <View>
+            <Text>Supa<span style={styles.header}>Menu</span></Text>
+        </View>
+    </View>
+}
+
+const styles = StyleSheet.create({
+   header: {
+       color: "#F7941D"
+   }
+})
